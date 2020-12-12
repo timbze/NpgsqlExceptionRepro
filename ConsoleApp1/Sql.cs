8e3f25ca179d4c50d99dd5348b3d4bca2d90df65
timbze <tim.bze@gmail.com>
2020-12-12 10:20:15 -0600

    Sql fix

--- ConsoleApp1/Sql.cs
+++ ConsoleApp1/Sql.cs
@@ -14,7 +14,7 @@ CREATE TABLE public.test (
 	location1 geography NULL,
 	location2 geography NULL,
 	value int4 NULL,
-	CONSTRAINT pk_test PRIMARY KEY (id);
+	CONSTRAINT pk_test PRIMARY KEY (id));
 
 INSERT INTO public.test (location1,location2,value) VALUES('POINT(-118.4079 33.9434)','POINT(-118.4079 33.9434)',98);
 INSERT INTO public.test (location1,location2,value) VALUES('POINT(-118.4079 33.9434)','POINT(-118.4079 33.9434)',98);
